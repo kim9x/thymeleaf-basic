0bf1862f4b5c3098689d67d37386370cf84cac97
Kim9xd <kim9xd@gmail.com>
2022-02-14 15:58:30 +0900

    * 타임리프 - 기본 기능   - 변수 - SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,14 @@
 package hello.thymeleaf.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
 
 @Controller
 @RequestMapping("/basic")
@@ -19,6 +24,38 @@ public class BasicController {
 	public String textUnescaped(Model model) {
 		model.addAttribute("data", "Hello <b>Spring!</b>");
 		return "basic/text-unescaped";
+	}
+	
+	@GetMapping("/variable")
+	public String variable(Model model) {
+		User userA = new User("userA", 10);
+		User userB = new User("userB", 20);
+		
+		ArrayList<User> list = new ArrayList<>();
+		list.add(userA);
+		list.add(userB);
+		
+		HashMap<String, User> map = new HashMap<>();
+		map.put("userA", userA);
+		map.put("userB", userB);
+		
+		model.addAttribute("user", userA);
+		model.addAttribute("users", list);
+		model.addAttribute("userMap", map);
+		
+		return "basic/variable";
+	}
+	
+	@Data
+	static class User {
+		private String username;
+		private int age;
+		
+		public User(String username, int age) {
+			super();
+			this.username = username;
+			this.age = age;
+		}
 	}
 
 }
